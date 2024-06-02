d7e870dfc3ea99e33643da2bceb8c4b71e45dcd6
m3rbi <yuval.meerbaum@island.io>
2024-06-02 09:15:08 +0300

    Add print utils for node and list

---
+++ io/print_utils.h
@@ -0,0 +1,20 @@
+#ifndef IO_PRINT_UTILS_H_
+#define IO_PRINT_UTILS_H_
+
+#include "../data_structures/linked_list.h"
+
+/**
+ * Prints the details of a node.
+ *
+ * @param node A pointer to the node to be printed.
+ */
+void print_node(node_t *node);
+
+/**
+ * Prints the details of a linked list.
+ *
+ * @param list A pointer to the linked list to be printed.
+ */
+void print_list(linked_list_t *list);
+
+#endif // IO_PRINT_UTILS_H_
